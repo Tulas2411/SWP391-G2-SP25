cc4d07f8a804bbcf986140d74b3e04b48d8ed721
dteedee <tungduongvbhp@gmail.com>
2025-03-17 07:42:22 +0700

    update change log customer

---
+++ Project-SWP391-G2-SP25/src/java/Model/UserLog.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class UserLog {
+    private int id;
+    private String log;
+    private int userId;
+    private Timestamp change_date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+    public Timestamp getChange_date() {
+        return change_date;
+    }
+
+    public void setChange_date(Timestamp change_date) {
+        this.change_date = change_date;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    
+}
