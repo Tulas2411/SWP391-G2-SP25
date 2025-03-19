89f7b907792e408e5dbcb142f909e53109568d6c
dteedee <tungduongvbhp@gmail.com>
2025-03-19 15:47:04 +0700

    fix config

--- Project-SWP391-G2-SP25/src/java/com/vnpay/common/Config.java
+++ Project-SWP391-G2-SP25/src/java/com/vnpay/common/Config.java
@@ -22,7 +22,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class Config {
 
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_ReturnUrl = "http://localhost:8080/OnlineShopSWD_Group6/CartCompletion.jsp";
+    public static String vnp_ReturnUrl = "http://localhost:9999/Project-SWP391-G2-SP25/CartCompletion.jsp";
     public static String vnp_TmnCode = "4YUP19I4";
     public static String secretKey = "MDUIFDCRAKLNBPOFIAFNEKFRNMFBYEPX";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
